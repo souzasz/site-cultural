c587db670d01c8cf7ba38aa3a277c86348c591ed
souzasz <106085286+souzasz@users.noreply.github.com>
2024-06-19 13:18:56 -0300

    js

--- script.js
+++ script.js
@@ -32,5 +32,8 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;    
 }
+
+mostraPergunta();
