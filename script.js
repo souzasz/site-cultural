7b2714a5fa7dc63128ef03f674620e3e9a9512d8
souzasz <106085286+souzasz@users.noreply.github.com>
2024-06-19 13:35:41 -0300

    js

--- script.js
+++ script.js
@@ -33,7 +33,16 @@ let perguntaAtual;
 
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;    
+    caixaPerguntas.textContent = perguntaAtual.enunciado;   
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(cosnt alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 mostraPergunta();
